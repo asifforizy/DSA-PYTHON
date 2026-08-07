19720834eb6e7863e50907ddc7e11ab2f325a7d2
Asif Forizy <asifforizy@gmail.com>
2026-08-07 10:09:17 +0600

    optimal solution

--- Basic python/print-factors.py
+++ Basic python/print-factors.py
@@ -13,7 +13,7 @@ print(factors(num))
 
 #! better  solution
 
-def optimalFactors(num):
+def betterFactors(num):
     result = []
     for i in range(1,num // 2):
         if num % i == 0:
@@ -22,5 +22,25 @@ def optimalFactors(num):
     return result
 
 num = 30
+
+print(betterFactors(num))
+
+
+#! optimal solution
+
+from math import sqrt
+
+def optimalFactors(num):
+    result = []
+    for i in range(1, int(sqrt(num))+1):
+        if num % i == 0:
+            result.append(i)
+            
+            if num//i !=i:
+                result.append(num//i)
+    result.sort()
+    return result
+
+num = 36
 
 print(optimalFactors(num))
