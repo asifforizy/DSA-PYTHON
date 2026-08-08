31b0ec3b70f879548d4495f7929ac604db69a1b0
Asif Forizy <asifforizy@gmail.com>
2026-08-08 23:18:33 +0600

    more optimal than previous

--- Basic python/hashing.py
+++ Basic python/hashing.py
@@ -10,3 +10,18 @@ for i in  m:
         if x == i:
             count+= 1
     print(count)
+    
+
+
+# optimal
+    
+hashList =[0]*11
+
+for num in n:
+    hashList[num] +=1
+    
+for num in m:
+    if num<1 or num> 10:
+        print(0)
+    else:
+        print(hashList[num])
