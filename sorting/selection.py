1582494de8745bf07c844f025cd47eab404797fd
Asif Forizy <asifforizy@gmail.com>
2026-08-14 17:45:00 +0600

    selection sort using function

--- sorting/selection.py
+++ sorting/selection.py
@@ -17,3 +17,19 @@ def selection_sort(arr):
 arr = [64, 25, 12, 22, 11]
 
 print(selection_sort(arr))
+
+
+
+def selection(nums):
+    n = len(nums)
+    for i in range(0,n):
+        min_index = i
+        for j in range (i+1,n):
+            if nums[j]<nums[min_index]:
+                min_index = j
+        nums[i],nums[min_index]= nums[min_index],nums[i]
+    return nums        
+        
+                
+print(selection([2,35,7,4,3,6,0]))
+    
