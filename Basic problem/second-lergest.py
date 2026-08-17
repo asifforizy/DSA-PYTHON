d7087d00b7ade6c30b49d22dca6cfab6f5b73d4e
Asif Forizy <asifforizy@gmail.com>
2026-08-17 11:03:58 +0600

    second lergest 2

---
+++ Basic problem/second-lergest.py
@@ -0,0 +1,34 @@
+nums = [3, 6, 8, 4, 8, 3, 7, 8, 4, 7, 6, 8, 5]
+
+largest = nums[0]
+second_largest = float('-inf')
+
+for i in range(1, len(nums)):
+    if nums[i] > largest:
+        second_largest = largest
+        largest = nums[i]
+    elif nums[i] > second_largest and nums[i] != largest:
+        second_largest = nums[i]
+
+print("Largest:", largest)
+print("Second largest:", second_largest)
+
+
+def second_largest(nums):
+    l = float('-inf')
+    sl = float('-inf')
+    n = len(nums)
+
+    for i in range(n):
+        l = max(l, nums[i])
+
+    for i in range(n):
+        if nums[i] > sl and nums[i] != l:
+            sl = nums[i]
+
+    return sl
+
+
+nums = [2, 4, 7, 4, 2, 7, 4, 9, 11, 35]
+
+print(second_largest(nums))
