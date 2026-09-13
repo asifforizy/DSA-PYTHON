c76f993c7af87763903e42f2a1c9bb986e28b3c1
Asif Forizy <asifforizy@gmail.com>
2026-09-13 09:25:19 +0600

    bouble sort optimizased

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -16,3 +16,31 @@ print(bubble_sort(arr))
 
 
 
+def bbs(nums):
+    n = len(nums)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if nums[j]>nums[j+1]:
+                nums[j],nums[j+1]=nums[j+1],nums[j]
+    return nums
+
+
+nums= [5,3,8,4,2]
+print(bbs(nums))
+
+
+def bubble_sort(nums):   
+    n = len(nums)
+    for i in range(n-2,-1,-1):
+        is_sorted = True
+        for j in range(0,i+1):
+            if nums[j]>nums[j+1]:
+                nums[j],nums[j+1]=nums[j+1],nums[j]
+                is_sorted = False
+        if is_sorted:
+            break
+            
+    return nums
+
+nums= [5,3,8,4,2,5,7,9,3,5,7]
+print(bubble_sort(nums))
